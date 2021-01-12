6485bc921b410f42e71aa031f4233d04e3047fa5
onethegarden <onethegarden@gmail.com>
2021-01-12 16:17:49 +0900

    토큰 재발급, 로그아웃

--- blog-backend/src/api/auth/auth.ctrl.js
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -100,5 +100,12 @@ export const check = async (ctx) => {
   }
   ctx.body = user;
 };
+
 //로그아웃
-export const logout = async (ctx) => {};
+/*
+  POST /api/auth/logout
+*/
+export const logout = async (ctx) => {
+  ctx.cookies.set('access_token');
+  ctx.status = 204;// No Content
+};
