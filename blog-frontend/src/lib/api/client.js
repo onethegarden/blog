56e65019c482bc8567f23cdd41b1473b91600a35
onethegarden <onethegarden@gmail.com>
2021-01-22 07:53:03 +0900

    proxy설정, api 함수 작성

---
+++ blog-frontend/src/lib/api/client.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const client = axios.create();
+
+/*
+글로벌 설정 예시
+
+//Client.defaults.baseURL = 'https://external-api-server.com/'
+
+//헤더설정
+client.default.headers.common['Authorization'] = 'Bearer a1b2c3d4';
+
+//인터셉터 설정
+axios.intercepter.response.use(\
+    response => {
+        //요청 성공 시 특정 작업 수행
+    },
+    error =>  {
+        //요청 실패 시 특정 작업 수행
+        return Promise.reject(error)
+    })
+*/
--- blog-frontend/src/modules/auth.js
+++ blog-frontend/src/modules/auth.js
@@ -1,8 +1,18 @@
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import {createRequestActionTypes} from '../lib/createRequestSaga';
 
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+/*
+const REGISTER = 'auth/REGISTER';
+const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
+const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+*/
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes('auth/REGISTER');
+
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes('auth/LOGIN');
 
 export const changeField = createAction(
   CHANGE_FIELD,
