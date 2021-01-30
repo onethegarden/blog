f499555c86fe4f30321cf1233cd286daa54ec252
onethegarden <onethegarden@gmail.com>
2021-01-30 11:27:43 +0900

    header 적용

---
+++ blog-frontend/src/components/common/Header.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import Responsive from './Responsive';
+import Button from './Button';
+
+const HeaderBlock = styled.div`
+  position: fixed;
+  width: 100%;
+  background: white;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
+`;
+/**
+ *  Responsive 컴포넌트 속성에 스타일 추가
+ */
+const Wrapper = styled(Responsive)`
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .logo {
+    font-size: 1.125rem;
+    font-weight: 800;
+    letter-spacing: 2px;
+  }
+  .right {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+//헤더가 fixed로 되어 있기 때문에 페이지의 콘텐츠가 4rem아래에 나타나도록
+const Spacer = styled.div`
+  height: 4rem;
+`;
+
+const Header = () => {
+  return (
+    <>
+      <HeaderBlock>
+        <Wrapper>
+          <div className="logo"> REACTERS</div>
+          <div className="right">
+            <Button>로그인</Button>
+          </div>
+        </Wrapper>
+      </HeaderBlock>
+      <Spacer />
+    </>
+  );
+};
+
+export default Header;
