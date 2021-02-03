04e26461ac773ee0ed59a544f80c5d9a8c7e9ade
onethegarden <onethegarden@gmail.com>
2021-02-04 07:50:44 +0900

    로그아웃 구현

--- blog-frontend/src/modules/user.js
+++ blog-frontend/src/modules/user.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import { takeLatest } from 'redux-saga/effects';
+import { takeLatest, call } from 'redux-saga/effects';
 import * as authAPI from '../lib/api/auth';
 import createRequestSaga, {
   createRequestActionTypes,
@@ -10,13 +10,34 @@ const TEMP_SET_USER = 'user/TEMP_SET_USER'; //새로고침 이후 임시 로그�
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
   'user/CHECK',
 );
-
+const LOGOUT = 'user/LOGOUT';
 export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
 export const check = createAction(CHECK);
+export const logout = createAction(LOGOUT);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
+
+function checkFailureSaga() {
+  try {
+    localStorage.removeItem('user'); //로컬스토리지에서 user제거
+  } catch (e) {
+    console.log('localStorage is not working');
+  }
+}
+
+function* logoutSaga() {
+  try {
+    yield call(authAPI.logout); //logoutAPI호출
+    localStorage.removeItem('user'); //localstorgae에서 제거
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
+  yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+  yield takeLatest(LOGOUT, logoutSaga);
 }
 
 const initialState = {
@@ -39,6 +60,10 @@ export default handleActions(
       ...state,
       user: null,
       checkError: error,
+    }),
+    [LOGOUT]: (state) => ({ //리듀서의 스토어의 값을 null로 설정
+      ...state,
+      user: null,
     }),
   },
   initialState,
