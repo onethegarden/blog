f589349f8cf05709dbfb5d4f4627a4b6a8e07157
onethegarden <onethegarden@gmail.com>
2021-02-15 00:21:19 +0900

    tagBox 등록삭제

--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Editor from '../components/write/Editors';
+import TagBox from '../components/write/TagBox';
 import Responsive from '../components/common/Responsive';
 
 const PostPage = () => {
     return (
     <Responsive>
         <Editor/>
+        <TagBox/>
     </Responsive>
         )
 }
