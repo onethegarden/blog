f6ed6c77b59cb04f6bcbf9dd01ace028e2ffd167
onethegarden <onethegarden@gmail.com>
2021-03-07 12:14:43 +0900

    write ActionButton 만들기

--- blog-frontend/src/components/common/Button.js
+++ blog-frontend/src/components/common/Button.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
-//import { withRouter } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
 
-const buttonStyle = `
+const buttonStyle = css`
   border: none;
   border-radius: 4px;
   font-size: 1rem;
@@ -13,47 +12,42 @@ const buttonStyle = `
   color: white;
   outline: none;
   cursor: pointer;
-
   background: ${palette.gray[8]};
   &:hover {
     background: ${palette.gray[6]};
   }
-
-  ${(props) =>
+  ${props =>
     props.fullWidth &&
-    `
-        padding-top: 0.75rem;
-        padding-bottom: 0.75rem;
-        width: 100%;
-        font-size: 1.125rem;
+    css`
+      padding-top: 0.75rem;
+      padding-bottom: 0.75rem;
+      width: 100%;
+      font-size: 1.125rem;
     `}
-  ${(props) =>
+  ${props =>
     props.cyan &&
-    `
-        background: ${palette.cyan[5]};
-        &:hover {
-            ackground: ${palette.cyan[4]};
-        }
+    css`
+      background: ${palette.cyan[5]};
+      &:hover {
+        background: ${palette.cyan[4]};
+      }
     `}
 `;
 
 const StyledButton = styled.button`
-  ${buttonStyle} //기존에 사용하던 스타일을 buttonStyle에 담아서 재사용 
+  ${buttonStyle}
 `;
 
 const StyledLink = styled(Link)`
   ${buttonStyle}
 `;
 
-//styled()함수로 만든 컴포넌트의 경우 임의 props가 필터릴ㅇ 되지 않음..
-//실제 엘리먼트에게 속성이 전달되지 않음,,, 
-//a태그는 boolean 값이 임의 props로 설정되는 것을 허용하지 않음, 숫자, 문자열만 허용 1163page 참조
-const Button = (props) => {
+const Button = props => {
   return props.to ? (
     <StyledLink {...props} cyan={props.cyan ? 1 : 0} />
   ) : (
     <StyledButton {...props} />
   );
 };
-//import가 제대로 작동하기 위해 styled-components로 만든 컴포넌트를 만들어서 export
+
 export default Button;
--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Editor from '../components/write/Editors';
 import TagBox from '../components/write/TagBox';
+import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
 
-const PostPage = () => {
+const WritePage = () => {
     return (
     <Responsive>
         <Editor/>
         <TagBox/>
+        <WriteActionButtons/>
     </Responsive>
         )
 }
 
-export default PostPage;
+export default WritePage;
